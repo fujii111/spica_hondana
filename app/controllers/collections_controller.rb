b3ec481f0179be397b3d0ca530f8a0484981c8b9
Koutarou Fujii <fujii@aica.ac.jp>
2015-03-10 16:27:43 +0900

    create collection from API

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,17 +3,19 @@ class CollectionsController < ApplicationController
   def index
     @collections = Collection.where("member_id = " + session[:id].to_s + " and state < 2")
       .order(state: :desc, regist_date: :desc)
-    @completed_collections = Collection.where(member_id: session[:id], state: 2)
-    @received_collections = Collection.where(request_member_id: session[:id], state: 2)
+    @completed_collections = Collection.where(member_id: session[:id], state: 8)
+    @received_collections = Collection.where(request_member_id: session[:id], state: 8)
     session[:url] = request.fullpath
   end
 
   def new
-    @book = nil
-    if params[:id]
+    if params[:id].present?
       @book = Book.find(params[:id])
     else
-#      @book = Book.find_by(isbn: params[:isbn], delete_flg: false)
+      @book = Book.find_by(isbn: params[:isbn], delete_flg: false)
+      if @book.blank?
+        @book = Book.getFromAPIByISBN(params[:isbn])
+      end
     end
     if session[:collection]
       @collection = Collection.new(session[:collection])
@@ -27,10 +29,10 @@ class CollectionsController < ApplicationController
 
   def confirm
     collection_params = params.require(:collection).permit(:book_id, :isbn, :condition, :band, :sunburn, :scratch, :cigar, :pet, :mold, :height, :width, :depth, :weight, :line, :broken, :note)
-    if params[:collection][:book_id]
+    if params[:collection][:book_id].present?
       @book = Book.find(params[:collection][:book_id])
     else
-#      @book = Book.find_by(isbn: params[:isbn], delete_flg: false)
+      @book = Book.getFromAPIByISBN(params[:collection][:isbn])
     end
     @collection = Collection.new(collection_params)
     if @collection.valid?
@@ -43,9 +45,21 @@ class CollectionsController < ApplicationController
 
   def create
     collection_params = params.require(:collection).permit(:book_id, :isbn, :condition, :band, :sunburn, :scratch, :cigar, :pet, :mold, :height, :width, :depth, :weight, :line, :broken, :note)
+    id = params[:collection][:book_id]
+    if params[:collection][:book_id].blank?
+      @book = Book.getFromAPIByISBN(params[:collection][:isbn])
+      @book.delete_flg = false
+      httpClient = HTTPClient.new
+      @book.image = httpClient.get_content(@book.image_url)
+      unless @book.save
+        render action: 'new'
+        return
+      end
+      id = @book.id
+    end
     @collection = Collection.new(collection_params)
     @collection.member_id = session[:id]
-    @collection.book_id = params[:collection][:book_id]
+    @collection.book_id = id
     @collection.state = 0
     @collection.regist_date = DateTime.now
     if @collection.save
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ActiveRecord::Base
   belongs_to :member
+  attr_accessor :image_url
 
   validates :title,
     presence: true
@@ -26,6 +27,34 @@ class Book < ActiveRecord::Base
 
   def data=(data)
     self.image = data.read
+  end
+
+  def self.getFromAPIByISBN(isbn)
+    begin
+      httpClient = HTTPClient.new
+      data = httpClient.get_content('https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522', {
+        'applicationId' => '1029724767561681573',
+        'affiliateId' => '12169043.4164998a.12169044.3519539e',
+        'format' => 'json',
+        'elements' => 'count,page,first,last,pageCount,title,author,publisherName,size,isbn,itemCaption,salesDate,itemUrl,largeImageUrl,booksGenreName',
+        'isbn' => isbn,
+        'hits' => '1'
+      })
+      json_data = JSON.parse data
+    rescue HTTPClient::BadResponseError => e
+    rescue HTTPClient::TimeoutError => e
+    end
+
+    book = Book.new
+    book.title = json_data["Items"][0]["Item"]["title"]
+    book.author = json_data["Items"][0]["Item"]["author"]
+    book.publisher = json_data["Items"][0]["Item"]["publisherName"]
+    book.language = "日本語"
+    book.sale_date = json_data["Items"][0]["Item"]["salesDate"]
+    book.description = json_data["Items"][0]["Item"]["itemCaption"]
+    book.image_url = json_data["Items"][0]["Item"]["largeImageUrl"]
+    book.isbn = isbn
+    return book
   end
 
   # TODO ファイル検証
