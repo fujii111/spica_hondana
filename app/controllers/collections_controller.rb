57ec086cdd5962692a0045690533e8be69f52c0a
Koutarou Fujii <fujii@aica.ac.jp>
2015-03-10 12:13:31 +0900

    confirm collection create

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -14,19 +14,35 @@ class CollectionsController < ApplicationController
     else
 #      @book = Book.find_by(isbn: params[:isbn], delete_flg: false)
     end
-    @collection = Collection.new
-    @collection.height = @book.height
-    @collection.width = @book.width
-    @collection.depth = @book.depth
+    if session[:collection]
+      @collection = Collection.new(session[:collection])
+    else
+      @collection = Collection.new
+      @collection.height = @book.height
+      @collection.width = @book.width
+      @collection.depth = @book.depth
+    end
   end
 
   def confirm
-    collection_params = params.require(:collection).permit(:book_id, :isbn, :data, :condition, :band, :sunburn, :scratch, :cigar, :pet, :mold, :height, :width, :depth, :weight, :line, :broken, :note)
+    collection_params = params.require(:collection).permit(:book_id, :isbn, :condition, :band, :sunburn, :scratch, :cigar, :pet, :mold, :height, :width, :depth, :weight, :line, :broken, :note)
+    if params[:collection][:book_id]
+      @book = Book.find(params[:collection][:book_id])
+    else
+#      @book = Book.find_by(isbn: params[:isbn], delete_flg: false)
+    end
     @collection = Collection.new(collection_params)
     @collection.member_id = session[:id]
     @collection.state = 0
     @collection.regist_date = DateTime.now
-    @collection.save
+    if @collection.valid?
+      session[:collection] = collection_params
+      render action: 'confirm'
+    else
+      render action: 'new'
+    end
   end
+
+
 
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,20 @@
 class Collection < ActiveRecord::Base
   attr_accessor :isbn
 
+  validates :width,
+    numericality: {greater_than: 0},
+    allow_blank: true
+
+  validates :height,
+    numericality: {greater_than: 0},
+    allow_blank: true
+
+  validates :depth,
+    numericality: {greater_than: 0},
+    allow_blank: true
+
+  validates :weight,
+    numericality: {greater_than: 0},
+    allow_blank: true
+
 end
